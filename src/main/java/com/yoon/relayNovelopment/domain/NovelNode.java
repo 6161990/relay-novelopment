f300a37eba7ba733ced40e82b57213186fe53689
Yoonji Kim <74708028+6161990@users.noreply.github.com>
2022-11-01 22:06:39 +0900

    node novel test 추가

--- src/main/java/com/yoon/relayNovelopment/domain/NovelNode.java
+++ src/main/java/com/yoon/relayNovelopment/domain/NovelNode.java
@@ -1,9 +1,12 @@
 package com.yoon.relayNovelopment.domain;
 
 import lombok.Getter;
+import org.valid4j.Validation;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static org.valid4j.Validation.validate;
 
 @Getter
 public class NovelNode {
@@ -21,6 +24,13 @@ public class NovelNode {
     }
 
     public void register(Novel novel) {
+        validate(novels.stream().noneMatch(n->n.getWriter().getId().equals(novel.getWriter().getId())),
+                new NovelNodeException(String.format("Already exist the writer. WriterId %s, NovelNodeId %s", novel.getWriter(), id)));
+        validate(novels.stream().noneMatch(n->n.getTitle().equals(novel.getTitle())),
+                new NovelNodeException(String.format("Already exist the title. WriterId %s, NovelNodeId %s", novel.getWriter(), id)));
+        validate(novels.stream().noneMatch(n->n.getId().equals(novel.getId())),
+                new NovelNodeException(String.format("Already exist the novel. NovelId %s, NovelNodeId %s", novel.getId(), id)));
+
         this.novels.add(novel);
     }
 }
--- src/test/java/com/yoon/relayNovelopment/domain/NovelNodeTest.java
+++ src/test/java/com/yoon/relayNovelopment/domain/NovelNodeTest.java
@@ -3,6 +3,7 @@ package com.yoon.relayNovelopment.domain;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class NovelNodeTest {
 
@@ -28,23 +29,45 @@ class NovelNodeTest {
     }
 
     @Test
-    void writer는_하나의_novelNode에_최초한번만_novel을_등록할_수_있다() {
+    void writer는_하나의_novelNode에_중복하여_등록할_수_없다() {
+        Novel novel = Novel.of(NovelId.of("id"), WriterId.of("writer"), Title.of("bang"), new Contents(), new Props());
+        NovelNode node = new NovelNode(NovelNodeId.of("nodeId1"), novel);
+        assertThat(node.getNovels().size()).isEqualTo(1);
 
+        Novel novel2 = Novel.of(NovelId.of("id2"), WriterId.of("writer"), Title.of("bang2"), new Contents(), new Props());
+
+        assertThatThrownBy(()-> node.register(novel2))
+                .isInstanceOf(NovelNodeException.class)
+                .hasMessage(String.format("Already exist the writer. WriterId %s, NovelNodeId %s", WriterId.of("writer"), NovelNodeId.of("nodeId1")));
     }
 
     @Test
     void 하나의_novelNode에_제목이_겹칠_수_없다() {
+        Novel novel = Novel.of(NovelId.of("id"), WriterId.of("writer"), Title.of("bang"), new Contents(), new Props());
+        NovelNode node = new NovelNode(NovelNodeId.of("nodeId1"), novel);
+        assertThat(node.getNovels().size()).isEqualTo(1);
 
+        Novel novel2 = Novel.of(NovelId.of("id2"), WriterId.of("writer2"), Title.of("bang"), new Contents(), new Props());
+
+        assertThatThrownBy(()-> node.register(novel2))
+                .isInstanceOf(NovelNodeException.class)
+                .hasMessage(String.format("Already exist the title. WriterId %s, NovelNodeId %s", WriterId.of("writer2"), NovelNodeId.of("nodeId1")));
     }
 
     @Test
     void 하나의_novelNode는_중복된_Novel이_등록될_수_없다() {
-        
+        // 질문 novelId 가 같다면 도메인까지 못오게 해야하는 것 아님? -> 어플리케이션에서 할 일.. ?
+        Novel novel = Novel.of(NovelId.of("id"), WriterId.of("writer"), Title.of("bang"), new Contents(), new Props());
+        NovelNode node = new NovelNode(NovelNodeId.of("nodeId1"), novel);
+        assertThat(node.getNovels().size()).isEqualTo(1);
+
+        Novel novel2 = Novel.of(NovelId.of("id"), WriterId.of("writer2"), Title.of("bang2"), new Contents(), new Props());
+
+        assertThatThrownBy(()-> node.register(novel2))
+                .isInstanceOf(NovelNodeException.class)
+                .hasMessage(String.format("Already exist the novel. NovelId %s, NovelNodeId %s", NovelId.of("id"), NovelNodeId.of("nodeId1")));
     }
 
-    @Test
-    void name() {
-    }
 
     // TODO : 유저에 따라 node 를 추가시킬 수 있다. 등급이 높으면 이미 10개의 노드가 달려있는 노드 노벨에 추가할 수 있다.
 }
