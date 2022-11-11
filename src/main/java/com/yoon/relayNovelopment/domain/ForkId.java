63dff6e36da0094807832bf26b91aa3beea215b8
Yoonji Kim <74708028+6161990@users.noreply.github.com>
2022-11-11 17:35:19 +0900

    Update ForkId.java

--- src/main/java/com/yoon/relayNovelopment/domain/ForkId.java
+++ src/main/java/com/yoon/relayNovelopment/domain/ForkId.java
@@ -3,6 +3,6 @@ package com.yoon.relayNovelopment.domain;
 import lombok.Value;
 
 @Value(staticConstructor = "of")
-public class ForkId implements NovelId {
+public class ForkId {
     String id;
 }
