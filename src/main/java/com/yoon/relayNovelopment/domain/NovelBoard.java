ac5d45b4b0e2cf93f8af87eb8b64e95210053457
Yoonji Kim <74708028+6161990@users.noreply.github.com>
2022-11-15 22:29:17 +0900

    NovelStage -> NovelBoard

--- src/main/java/com/yoon/relayNovelopment/domain/NovelBoard.java
+++ src/main/java/com/yoon/relayNovelopment/domain/NovelBoard.java
@@ -2,7 +2,6 @@ package com.yoon.relayNovelopment.domain;
 
 import lombok.Getter;
 import lombok.ToString;
-import org.valid4j.Validation;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,44 +10,46 @@ import static org.valid4j.Validation.validate;
 
 @Getter
 @ToString
-public class NovelStage {
+public class NovelBoard {
     private final NovelStageId id;
     private final Opening opening;
     private final int maxStageSize;
 
     @Getter
-    private List<Step> steps;
+    private List<Stage> stages;
 
-    public NovelStage(NovelStageId id, Opening opening, int maxStageSize) {
+    public NovelBoard(NovelStageId id, Opening opening, int maxStageSize) {
         this.id = id;
         this.opening = opening;
         this.maxStageSize = maxStageSize;
     }
 
     public void relay(Fork fork) {
-        if (steps == null || steps.isEmpty()){
+        if (stages == null || stages.isEmpty()){
             init();
         }
 
-        valid(fork);
+        valid(fork); // TODO maxStageSize 가 일정 값 이상이면 throw
 
-        this.steps.get(steps.size() - 1).add(fork); // TODO 시점마다 FORK 가 적용될 수 있는 Realy 가 있다.
+        this.stages.get(stages.size() - 1).add(fork);
+        // TODO 시점마다 FORK 가 적용될 수 있는 Realy 가 있다.
+        // TODO 맨 마지막 steps 이 아니다.
     }
 
     private void init() {
-        this.steps = new ArrayList<>();
+        this.stages = new ArrayList<>();
         List<Fork> forks = new ArrayList<>();
-        steps.add(new Step(forks));
+        stages.add(new Stage(forks));
     }
 
     public long getNovelSize(){
-        return this.getSteps() == null ? 0 : this.steps.stream().mapToInt(e->e.getForks().size()).sum();
+        return this.getStages() == null ? 0 : this.stages.stream().mapToInt(e->e.getForks().size()).sum();
     }
 
     private void valid(Fork fork) {
-        validate(steps.stream().noneMatch(n-> n.getForks().stream().anyMatch(f->f.getWriterId().getId().equals(fork.getWriterId().getId()))),
+        validate(stages.stream().noneMatch(n-> n.getForks().stream().anyMatch(f->f.getWriterId().getId().equals(fork.getWriterId().getId()))),
                 new NovelNodeException(String.format("Already exist the writer. WriterId %s, NovelNodeId %s", fork.getWriterId(), id)));
-        validate(steps.stream().noneMatch(n-> n.getForks().stream().anyMatch(f->f.getTitle().equals(fork.getTitle()))),
+        validate(stages.stream().noneMatch(n-> n.getForks().stream().anyMatch(f->f.getTitle().equals(fork.getTitle()))),
                 new NovelNodeException(String.format("Already exist the title. WriterId %s, NovelNodeId %s", fork.getWriterId(), id)));
     }
 
