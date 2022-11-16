4e7df549fcb93724b377e7558a2605da009c9cb8
Yoonji Kim <74708028+6161990@users.noreply.github.com>
2022-11-16 22:07:01 +0900

    Fork -> Novel

--- src/main/java/com/yoon/relayNovelopment/domain/NovelBoard.java
+++ src/main/java/com/yoon/relayNovelopment/domain/NovelBoard.java
@@ -24,33 +24,33 @@ public class NovelBoard {
         this.maxStageSize = maxStageSize;
     }
 
-    public void relay(Fork fork) {
+    public void relay(Novel novel) {
         if (stages == null || stages.isEmpty()){
             init();
         }
 
-        valid(fork); // TODO maxStageSize 가 일정 값 이상이면 throw
+        valid(novel); // TODO maxStageSize 가 일정 값 이상이면 throw
 
-        this.stages.get(stages.size() - 1).add(fork);
+        this.stages.get(stages.size() - 1).add(novel);
         // TODO 시점마다 FORK 가 적용될 수 있는 Realy 가 있다.
         // TODO 맨 마지막 steps 이 아니다.
     }
 
     private void init() {
         this.stages = new ArrayList<>();
-        List<Fork> forks = new ArrayList<>();
-        stages.add(new Stage(forks));
+        List<Novel> novels = new ArrayList<>();
+        stages.add(new Stage(novels));
     }
 
     public long getNovelSize(){
-        return this.getStages() == null ? 0 : this.stages.stream().mapToInt(e->e.getForks().size()).sum();
+        return this.getStages() == null ? 0 : this.stages.stream().mapToInt(e->e.getNovels().size()).sum();
     }
 
-    private void valid(Fork fork) {
-        validate(stages.stream().noneMatch(n-> n.getForks().stream().anyMatch(f->f.getWriterId().getId().equals(fork.getWriterId().getId()))),
-                new NovelNodeException(String.format("Already exist the writer. WriterId %s, NovelNodeId %s", fork.getWriterId(), id)));
-        validate(stages.stream().noneMatch(n-> n.getForks().stream().anyMatch(f->f.getTitle().equals(fork.getTitle()))),
-                new NovelNodeException(String.format("Already exist the title. WriterId %s, NovelNodeId %s", fork.getWriterId(), id)));
+    private void valid(Novel novel) {
+        validate(stages.stream().noneMatch(n-> n.getNovels().stream().anyMatch(f->f.getWriterId().getId().equals(novel.getWriterId().getId()))),
+                new NovelNodeException(String.format("Already exist the writer. WriterId %s, NovelNodeId %s", novel.getWriterId(), id)));
+        validate(stages.stream().noneMatch(n-> n.getNovels().stream().anyMatch(f->f.getTitle().equals(novel.getTitle()))),
+                new NovelNodeException(String.format("Already exist the title. WriterId %s, NovelNodeId %s", novel.getWriterId(), id)));
     }
 
 }
--- src/main/java/com/yoon/relayNovelopment/domain/Stage.java
+++ src/main/java/com/yoon/relayNovelopment/domain/Stage.java
@@ -8,16 +8,16 @@ import java.util.List;
 public class Stage {
 
     private RelayId id;
-    private List<Fork> forks;
+    private List<Novel> novels;
     private StageState stageState;
     private boolean isClosed;
 
-    public Stage(List<Fork> forks) {
-        this.forks = forks;
+    public Stage(List<Novel> novels) {
+        this.novels = novels;
     }
 
-    public void add(Fork fork){
-        forks.add(fork);
+    public void add(Novel novel){
+        novels.add(novel);
     }
 
     public void setClosed(){
