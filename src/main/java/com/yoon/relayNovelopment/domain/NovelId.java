6f965c8075420dd1b5d3060fc56eed36e0314ce8
Yoonji Kim <74708028+6161990@users.noreply.github.com>
2022-11-17 23:46:19 +0900

    Update NovelId.java

--- src/main/java/com/yoon/relayNovelopment/domain/NovelId.java
+++ src/main/java/com/yoon/relayNovelopment/domain/NovelId.java
@@ -1,11 +1,4 @@
 package com.yoon.relayNovelopment.domain;
 
 public interface NovelId {
-    static NovelId relayNovelIdOf(String id) {
-        return RelayNovelId.of(id);
-    }
-
-    static NovelId openingIdOf(String id) {
-        return OpeningId.of(id);
-    }
 }
