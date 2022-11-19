ba6da99e887a058a87d27883155da8b004931df0
Yoonji Kim <74708028+6161990@users.noreply.github.com>
2022-11-19 21:14:34 +0900

    Update NovelBoardTest.java

--- src/test/java/com/yoon/relayNovelopment/domain/NovelBoardTest.java
+++ src/test/java/com/yoon/relayNovelopment/domain/NovelBoardTest.java
@@ -28,29 +28,51 @@ class NovelBoardTest {
                 .opening(openingId("id"), writerId("writer"),
                         title("bang"), content("contentId", "content"))
                 .maxRelay(MAX_RELAY_SIZE_5).build();
+        novelStage.relay(novel(relayId("id2"), openingId("id"), writerId("writer2"), title("bang2")));
 
-        novelStage.relay(Novel.of(relayId("id2"), openingId("id"), writerId("writer2"), title("bang2")));
-
-        novelStage.relay(Novel.of(relayId("id3"), parentId("id2"), writerId("writer3"), title("bang3")));
+        novelStage.relay(novel(relayId("id3"), parentId("id2"), writerId("writer3"), title("bang3")));
 
         assertThat(novelStage.getNovelSize()).isEqualTo(2);
     }
 
     @Test
-    void 부모novelId가_무엇인지를_찾아야한다() {
-            // 혹은 연결된 이전 노드가 무엇인지 찾아야한다 ---------- HERE !!!!!!!!!!!
-        Novel novel2 = Novel.of(RelayNovelId.of("id2"), OpeningId.of("id"), WriterId.of("writer2"), Title.of("bang2"));
-    }
-
-    @Test
-    void add_fork() {
-        // 부모 novelId가 일치한 형제노드가 반드시 존재한다.
-
+    void 부모novelId가_무엇인지를_찾아야한다() { // FIXME 바깥 레이어의 test 같다.
+            // 혹은 연결된 이전 노드가 무엇인지 찾아야한다 openingId ? parentId ---------- HERE !!!!!!!!!!!
+        Novel novel2 = novel(RelayNovelId.of("id2"), OpeningId.of("id"), WriterId.of("writer2"), Title.of("bang2"));
     }
 
     @Test
     void when_exceed_maxRelayCount_add_relay() {
 
+    }
+
+    @Test
+    void add_fork() {
+        NovelBoard novelStage = NovelBoardBuilder.builder()
+                .id("id")
+                .opening(openingId("id"), writerId("writer"),
+                        title("bang"), content("contentId", "content"))
+                .maxRelay(MAX_RELAY_SIZE_5).build();
+        novelStage.relay(novel(relayId("id2"), openingId("id"), writerId("writer2"), title("bang2")));
+
+        novelStage.fork(novel(relayId("id3"), openingId("id"), writerId("writer3"), title("bang4")));
+
+        assertThat(novelStage.getNovelSize()).isEqualTo(2);
+        assertThat(novelStage.getSameParentSizeBy(openingId("id"))).isEqualTo(2);
+    }
+
+    @Test
+    void when_not_exist_same_parentNovel_add_fork() {
+        NovelBoard novelStage = NovelBoardBuilder.builder()
+                .id("id")
+                .opening(openingId("id"), writerId("writer"),
+                        title("bang"), content("contentId", "content"))
+                .maxRelay(MAX_RELAY_SIZE_5).build();
+        novelStage.relay(novel(relayId("id2"), openingId("id"), writerId("writer2"), title("bang2")));
+
+        assertThatThrownBy(()->novelStage.fork(novel(relayId("id3"), openingId("id2"), writerId("writer3"), title("bang4"))))
+                .isInstanceOf(NovelNodeException.class)
+                .hasMessage("Not Exist Same Parent Novel.");
     }
 
     @Test
@@ -60,10 +82,9 @@ class NovelBoardTest {
                 .opening(openingId("id"), writerId("writer"),
                         title("bang"), content("contentId", "content"))
                 .maxRelay(MAX_RELAY_SIZE_5).build();
+        novelStage.relay(novel(relayId("id2"), openingId("id"), writerId("writer2"), title("bang2")));
 
-        novelStage.relay(Novel.of(relayId("id2"), openingId("id"), writerId("writer2"), title("bang2")));
-
-        assertThatThrownBy(()-> novelStage.relay(Novel.of(relayId("id3"), parentId("id2"), writerId("writer2"), title("bang2"))))
+        assertThatThrownBy(()-> novelStage.relay(novel(relayId("id3"), parentId("id2"), writerId("writer2"), title("bang2"))))
                 .isInstanceOf(NovelNodeException.class)
                 .hasMessage(String.format("Already exist the writer. WriterId %s, NovelBoardId %s", WriterId.of("writer2"), NovelBoardId.of("id")));
 
@@ -76,12 +97,15 @@ class NovelBoardTest {
                 .opening(openingId("id"), writerId("writer"),
                         title("bang"), content("contentId", "content"))
                 .maxRelay(MAX_RELAY_SIZE_5).build();
+        novelStage.relay(novel(relayId("id2"), openingId("id"), writerId("writer2"), title("bang2")));
 
-        novelStage.relay(Novel.of(relayId("id2"), openingId("id"), writerId("writer2"), title("bang2")));
-
-        assertThatThrownBy(()-> novelStage.relay(Novel.of(relayId("id3"), parentId("id2"), writerId("writer3"), title("bang2"))))
+        assertThatThrownBy(()-> novelStage.relay(novel(relayId("id3"), parentId("id2"), writerId("writer3"), title("bang2"))))
                 .isInstanceOf(NovelNodeException.class)
                 .hasMessage(String.format("Already exist the title. WriterId %s, NovelBoardId %s", WriterId.of("writer3"), NovelBoardId.of("id")));
+    }
+
+    private Novel novel(RelayNovelId id, NovelId parentId, WriterId writerId, Title title) {
+        return Novel.of(id, parentId, writerId, title, Content.of(ContentId.of("conId"), "value"), new Props());
     }
 
 }
