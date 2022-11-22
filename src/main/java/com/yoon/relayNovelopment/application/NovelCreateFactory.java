9d290182e4e0333885a11846addc867f20ab0d54
Yoonji Kim <74708028+6161990@users.noreply.github.com>
2022-11-22 19:51:25 +0900

    FIXME idgen 을 통한 RelayNovelId

--- src/main/java/com/yoon/relayNovelopment/application/NovelCreateFactory.java
+++ src/main/java/com/yoon/relayNovelopment/application/NovelCreateFactory.java
@@ -15,7 +15,14 @@ public class NovelCreateFactory {
     public Novel create(NovelBoard novelBoard, NovelEditCommand command) {
         NovelId parentId = parentFinder.getParentBy(novelBoard);
 
-        return Novel.of(RelayNovelId.of(idGenerator.gen()), parentId,
+        return Novel.of(RelayNovelId.of("boardId"), parentId,
                         command.getWriterId(), command.getTitle(), command.getContent(), command.getProps());
+    }
+
+    public Novel createForFork(NovelBoard novelBoard, NovelEditCommand command) {
+        NovelId parentId = parentFinder.getParentForForkBy(novelBoard);
+
+        return Novel.of(RelayNovelId.of("boardId"), parentId, // FIXME idgen 을 통한 RelayNovelId
+                command.getWriterId(), command.getTitle(), command.getContent(), command.getProps());
     }
 }
