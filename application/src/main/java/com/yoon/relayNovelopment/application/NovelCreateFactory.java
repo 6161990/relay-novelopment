7ab5133b1096b07545a56d3eaae73df07b64263e
Yoonji Kim <74708028+6161990@users.noreply.github.com>
2022-11-28 10:39:56 +0900

    novel 은 id 가 아니라 key 로 생성한다. TODO : 어떤 조합??

--- application/src/main/java/com/yoon/relayNovelopment/application/NovelCreateFactory.java
+++ application/src/main/java/com/yoon/relayNovelopment/application/NovelCreateFactory.java
@@ -11,19 +11,19 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class NovelCreateFactory {
 
-    private final IdGeneratore idGeneratore;
+    private final IdGenerator idGenerator;
 
     public Novel create(NovelBoard novelBoard, NovelCreateCommand command) {
         NovelId parentId = ParentNovelFinder.getParentBy(novelBoard);
 
-        return Novel.of(RelayNovelId.of("boardId"), parentId,
+        return Novel.of(RelayNovelId.of(idGenerator.getId()), parentId,
                         command.getWriterId(), command.getTitle(), command.getContent());
     }
 
     public Novel createForFork(NovelBoard novelBoard, NovelCreateCommand command) {
         NovelId parentId = ParentNovelFinder.getParentForForkBy(novelBoard);
 
-        return Novel.of(RelayNovelId.of("boardId"), parentId, // FIXME idgen 을 통한 RelayNovelId
+        return Novel.of(RelayNovelId.of(idGenerator.getId()), parentId, // FIXME idgen 을 통한 RelayNovelId id 말고 key 로 !
                 command.getWriterId(), command.getTitle(), command.getContent());
     }
 }
