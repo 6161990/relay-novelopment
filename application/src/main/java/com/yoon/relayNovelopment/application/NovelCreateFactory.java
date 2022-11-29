f33e057f4163f12bfb6fabb35c35b085c6e66750
Yoonji Kim <74708028+6161990@users.noreply.github.com>
2022-11-29 22:13:43 +0900

    create NovelCommand

--- application/src/main/java/com/yoon/relayNovelopment/application/NovelCreateFactory.java
+++ application/src/main/java/com/yoon/relayNovelopment/application/NovelCreateFactory.java
@@ -1,9 +1,7 @@
 package com.yoon.relayNovelopment.application;
 
-import com.yoon.relayNovelopment.domain.Novel;
-import com.yoon.relayNovelopment.domain.NovelBoard;
-import com.yoon.relayNovelopment.domain.NovelId;
-import com.yoon.relayNovelopment.domain.RelayNovelId;
+import com.yoon.relayNovelopment.config.IdGenerator;
+import com.yoon.relayNovelopment.domain.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,17 +11,17 @@ public class NovelCreateFactory {
 
     private final IdGenerator idGenerator;
 
-    public Novel create(NovelBoard novelBoard, NovelCreateCommand command) {
+    public Novel create(NovelBoard novelBoard, NovelCommand command) {
         NovelId parentId = ParentNovelFinder.getParentBy(novelBoard);
 
         return Novel.of(RelayNovelId.of(idGenerator.getId()), parentId,
                         command.getWriterId(), command.getTitle(), command.getContent());
     }
 
-    public Novel createForFork(NovelBoard novelBoard, NovelCreateCommand command) {
+    public Novel createForFork(NovelBoard novelBoard, NovelCommand command) {
         NovelId parentId = ParentNovelFinder.getParentForForkBy(novelBoard);
 
-        return Novel.of(RelayNovelId.of(idGenerator.getId()), parentId, // FIXME idgen 을 통한 RelayNovelId id 말고 key 로 !
+        return Novel.of(RelayNovelId.of(idGenerator.getId()), parentId, // FIXME idgen 을 통한 RelayNovelId id 말고 key 로 ! 왜냐면 idGen 으로 만들기에는 과하다..
                 command.getWriterId(), command.getTitle(), command.getContent());
     }
 }
