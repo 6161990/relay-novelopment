872f46dc71c47ef3f16ce3676c73cb125946d87a
Yoonji Kim <74708028+6161990@users.noreply.github.com>
2022-12-04 10:43:38 +0900

    가라

--- application/src/main/java/com/yoon/relayNovelopment/application/NovelBoardCreatorController.java
+++ application/src/main/java/com/yoon/relayNovelopment/application/NovelBoardCreatorController.java
@@ -2,15 +2,11 @@ package com.yoon.relayNovelopment.application;
 
 import com.yoon.relayNovelopment.domain.NovelBoard;
 import com.yoon.relayNovelopment.service.NovelBoardCreator;
-import com.yoon.relayNovelopment.service.NovelBoardEditor;
 import com.yoon.relayNovelopment.service.NovelCreateCommand;
-import com.yoon.relayNovelopment.service.NovelEditorCommand;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
