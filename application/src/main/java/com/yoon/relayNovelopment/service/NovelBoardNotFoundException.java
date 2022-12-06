61a6a9260aae9ed8a0fbc5d50e30d922810d77d4
Yoonji Kim <74708028+6161990@users.noreply.github.com>
2022-12-07 08:50:00 +0900

    viewController

---
+++ application/src/main/java/com/yoon/relayNovelopment/service/NovelBoardNotFoundException.java
@@ -0,0 +1,8 @@
+package com.yoon.relayNovelopment.service;
+
+public class NovelBoardNotFoundException extends RuntimeException {
+
+    public NovelBoardNotFoundException(String message) {
+        super(message);
+    }
+}
--- application/src/test/java/com/yoon/relayNovelopment/application/NovelBoardViewControllerTest.java
+++ application/src/test/java/com/yoon/relayNovelopment/application/NovelBoardViewControllerTest.java
@@ -1,12 +1,15 @@
 package com.yoon.relayNovelopment.application;
 
 import com.yoon.relayNovelopment.domain.*;
+import com.yoon.relayNovelopment.service.NovelBoardNotFoundException;
 import com.yoon.relayNovelopment.service.NovelBoardViewer;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.mockito.BDDMockito.given;
@@ -40,12 +43,31 @@ class NovelBoardViewControllerTest {
     }
 
     @Test
-    void detailFineAll() {
+    void detailFineAll() throws Exception {
+        NovelBoard novelBoard
+                = new NovelBoard(NovelBoardId.of("1004"), Opening.of(OpeningKey.of("openKey"), WriterId.of("writer"), Title.of("title"), Content.of("content")));
+        NovelBoard novelBoard1
+                = new NovelBoard(NovelBoardId.of("1005"), Opening.of(OpeningKey.of("openKey1"), WriterId.of("writer1"), Title.of("title1"), Content.of("content1")));
+        given(novelBoardViewer.findAll()).willReturn(List.of(novelBoard, novelBoard1));
 
+        mvc.perform(get("/novelBoards"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(
+                        containsString("{\"novelBoardId\":{\"id\":\"1004\"}")
+                ))
+                .andExpect(content().string(
+                        containsString("{\"novelBoardId\":{\"id\":\"1005\"}")
+                ));
     }
 
     @Test
-    void detailWithNotExisted() {
+    void detailWithNotExisted() throws Exception {
+        given(novelBoardViewer.findBy(NovelBoardId.of("404")))
+                .willThrow(new NovelBoardNotFoundException("This NovelBoard Not Found. NovelBoardId is 404."));
+
+        mvc.perform(get("/novelBoard/404"))
+                .andExpect(status().isNotFound())
+                .andExpect(content().string("{}"));
     }
 
 }
