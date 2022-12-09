568a087382b9623510a5d4195c52eb2dde381a51
Yoonji Kim <74708028+6161990@users.noreply.github.com>
2022-12-09 09:18:31 +0900

    controller imple

--- application/src/main/java/com/yoon/relayNovelopment/application/NovelBoardEditorController.java
+++ application/src/main/java/com/yoon/relayNovelopment/application/NovelBoardEditorController.java
@@ -1,9 +1,8 @@
 package com.yoon.relayNovelopment.application;
 
 import com.yoon.relayNovelopment.domain.NovelBoard;
-import com.yoon.relayNovelopment.service.NovelBoardCreator;
+import com.yoon.relayNovelopment.domain.NovelBoardId;
 import com.yoon.relayNovelopment.service.NovelBoardEditor;
-import com.yoon.relayNovelopment.service.NovelCreateCommand;
 import com.yoon.relayNovelopment.service.NovelEditorCommand;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,24 +10,35 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @RestController
 @RequiredArgsConstructor
 public class NovelBoardEditorController {
 
-    // TODO commandConverter 와 command 와 service 를 어떻게 분리하지?
-
     @Autowired
     private final NovelBoardEditor novelBoardEditor;
 
-    @PatchMapping("/addNovel/{id}")
-    public ResponseEntity<NovelBoard> add(@PathVariable("id") String id,
-                         @Valid @RequestBody NovelEditorCommand novelEditorCommand){
-        return null;
+    @PostMapping("/relay/{id}")
+    public ResponseEntity<?> relay(@PathVariable("id") String id,
+                         @Valid @RequestBody NovelEditorCommand novelEditorCommand) throws URISyntaxException {
+        novelBoardEditor.relay(novelEditorCommand);
+        URI location = new URI("/novelBoard/"+id);
+        return ResponseEntity.created(location).body("{}");
+    }
+
+    @PostMapping("/fork/{id}")
+    public ResponseEntity<?> fork(@PathVariable("id") String id,
+                                          @Valid @RequestBody NovelEditorCommand novelEditorCommand) throws URISyntaxException {
+        novelBoardEditor.fork(novelEditorCommand);
+        URI location = new URI("/novelBoard/"+id);
+        return ResponseEntity.created(location).body("{}");
     }
 
     @DeleteMapping("/remove/{id}")
-    public NovelBoard remove(@PathVariable("id") String id){
-        return null;
+    public String remove(@PathVariable("id") NovelBoardId id){
+        novelBoardEditor.remove(id);
+        return "{}";
     }
 }
