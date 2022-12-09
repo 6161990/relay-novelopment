928a024cd9e82b90d4c7a859978c1c0246446623
Yoonji Kim <74708028+6161990@users.noreply.github.com>
2022-12-09 09:17:59 +0900

    rename

--- application/src/test/java/com/yoon/relayNovelopment/application/NovelBoardViewerControllerTest.java
+++ application/src/test/java/com/yoon/relayNovelopment/application/NovelBoardViewerControllerTest.java
@@ -18,7 +18,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(NovelBoardViewerController.class)
-class NovelBoardViewControllerTest {
+class NovelBoardViewerControllerTest {
 
     @Autowired
     MockMvc mvc;
