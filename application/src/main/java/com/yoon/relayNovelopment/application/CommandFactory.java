c2c56935a4005f089d31746670a07f0ef72329c6
Yoonji Kim <74708028+6161990@users.noreply.github.com>
2022-12-10 15:15:38 +0900

    Create CommandFactory.java

---
+++ application/src/main/java/com/yoon/relayNovelopment/application/CommandFactory.java
@@ -0,0 +1,16 @@
+package com.yoon.relayNovelopment.application;
+
+import com.yoon.relayNovelopment.domain.Content;
+import com.yoon.relayNovelopment.domain.Title;
+import com.yoon.relayNovelopment.domain.WriterId;
+import com.yoon.relayNovelopment.service.NovelCreateCommand;
+import com.yoon.relayNovelopment.service.NovelCreateRequest;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CommandFactory {
+
+    public NovelCreateCommand create(NovelCreateRequest request) {
+        return new NovelCreateCommand(WriterId.of(request.getWriterId()), Title.of(request.getTitle()), Content.of(request.getContent()));
+    }
+}
