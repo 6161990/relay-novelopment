8c11c896bdb702c08fb8f52c4a110a86b48e3e3f
Yoonji Kim <74708028+6161990@users.noreply.github.com>
2022-12-12 06:31:42 +0900

    Update CommandFactory.java

--- application/src/main/java/com/yoon/relayNovelopment/application/CommandFactory.java
+++ application/src/main/java/com/yoon/relayNovelopment/application/CommandFactory.java
@@ -4,18 +4,19 @@ import com.yoon.relayNovelopment.domain.Content;
 import com.yoon.relayNovelopment.domain.NovelBoardId;
 import com.yoon.relayNovelopment.domain.Title;
 import com.yoon.relayNovelopment.domain.WriterId;
+import com.yoon.relayNovelopment.service.NovelCommand;
 import com.yoon.relayNovelopment.service.NovelCreateCommand;
 import com.yoon.relayNovelopment.service.NovelEditorCommand;
 import org.springframework.stereotype.Component;
 
 @Component
-public class CommandFactory {
+public class CommandFactory { // FIXME : commandFactory 는 interface 이며 create / editor 따로 팩토리를 빼야한다.
 
-    public NovelCreateCommand create(NovelCreateRequest request) {
+    public NovelCommand createBy(NovelCreateRequest request) {
         return new NovelCreateCommand(WriterId.of(request.getWriterId()), Title.of(request.getTitle()), Content.of(request.getContent()));
     }
 
-    public NovelEditorCommand editor(NovelEditorRequest novelEditorRequest) {
-        return new NovelEditorCommand(NovelBoardId.of(novelEditorRequest.getNovelBoardId()),WriterId.of(novelEditorRequest.getWriterId()), Title.of(novelEditorRequest.getTitle()), Content.of(novelEditorRequest.getContent()));
+    public NovelCommand createBy(NovelBoardId id, NovelCreateRequest request) {
+        return new NovelEditorCommand(id, WriterId.of(request.getWriterId()), Title.of(request.getTitle()), Content.of(request.getContent()));
     }
 }
