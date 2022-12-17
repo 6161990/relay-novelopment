a289ec17e7c540c04c48fb6b2aa9340120c49563
Yoonji Kim <74708028+6161990@users.noreply.github.com>
2022-12-17 20:48:59 +0900

    test 유틸성으로 수정

--- application/src/test/java/com/yoon/relayNovelopment/config/DefaultIdGeneratorTest.java
+++ application/src/test/java/com/yoon/relayNovelopment/config/DefaultIdGeneratorTest.java
@@ -10,11 +10,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 class DefaultIdGeneratorTest {
 
     @Test
-    void key는_novelBoard_title_조합으로_만들어진다() {
+    void keyGen() {
         DefaultIdGenerator generator = new DefaultIdGenerator(new AlternativeJdkIdGenerator());
 
-        String key = generator.getKey(NovelBoardId.of("novelBoardId"), Title.of("그해 우리는"));
+        String key = generator.getKey(NovelBoardId.of("aaa"), Title.of("bbb"));
 
-        assertThat(key).isEqualTo("novelBoardId:그해 우리는");
+        assertThat(key).isEqualTo("aaa:bbb");
     }
 }
