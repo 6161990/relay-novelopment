b3a720d29fde38b9c0cb899283471efed8a0adf5
Yoonji Kim <74708028+6161990@users.noreply.github.com>
2022-12-18 11:20:20 +0900

    infra config

---
+++ infrastructure/src/main/java/com/yoon/relayNovelopment/config/SpringDataJdbcConfig.java
@@ -0,0 +1,27 @@
+package com.yoon.relayNovelopment.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.TransactionManager;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableJdbcRepositories
+public class SpringDataJdbcConfig {
+
+    @Bean
+    NamedParameterJdbcOperations jdbcOperations(DataSource dataSource) {
+        return new NamedParameterJdbcTemplate(dataSource);
+    }
+
+    @Bean
+    TransactionManager transactionManager(DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
+    }
+
+}
