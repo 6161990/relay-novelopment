48f4eb914755a2440e6d964995629262d4b7bc21
Yoonji Kim <74708028+6161990@users.noreply.github.com>
2022-12-19 11:59:50 +0900

    NovelBoardIdConverters

---
+++ infrastructure/src/main/java/com/yoon/relayNovelopment/config/NovelBoardIdConverters.java
@@ -0,0 +1,29 @@
+package com.yoon.relayNovelopment.config;
+
+import com.yoon.relayNovelopment.domain.NovelBoardId;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.data.convert.ReadingConverter;
+import org.springframework.data.convert.WritingConverter;
+
+public class NovelBoardIdConverters {
+    public static NovelBoardIdReadingConverter READING = new NovelBoardIdReadingConverter();
+    public static NovelBoardIdWritingConverter WRITING = new NovelBoardIdWritingConverter();
+
+    @ReadingConverter
+    private static class NovelBoardIdReadingConverter implements Converter<String, NovelBoardId> {
+
+        @Override
+        public NovelBoardId convert(String source) {
+            return NovelBoardId.of(source);
+        }
+    }
+
+    @WritingConverter
+    private static class NovelBoardIdWritingConverter implements Converter<NovelBoardId, String> {
+
+        @Override
+        public String convert(NovelBoardId source) {
+            return source.getId();
+        }
+    }
+}
