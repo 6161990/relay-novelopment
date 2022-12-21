fceb5a89fdf29be021f33fd3bd81fac412eab7c4
6161990 <6161990src@gmail.com>
2022-12-21 13:54:57 +0900

    delete 추가

---
+++ infrastructure/src/test/resources/relay-novel.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS relay_novel
+(
+    id                   VARCHAR(255) PRIMARY KEY,
+    opening           jsonb,
+    is_closed   boolean NOT NULL,
+    novels          jsonb,
+    created_at          TIMESTAMPTZ,
+    deleted_at          TIMESTAMPTZ,
+    version             BIGINT
+    );
+
+create unique index relay_novel_id_index
+    on "relay_novel" (id);
+
