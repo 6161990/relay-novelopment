ba0b875e2b8feb07e1bac015a88f2114ebf5048a
6161990 <6161990src@gmail.com>
2022-12-30 11:31:28 +0900

    캡슐화

--- domain/src/main/java/com/yoon/relayNovelopment/domain/Novels.java
+++ domain/src/main/java/com/yoon/relayNovelopment/domain/Novels.java
@@ -8,35 +8,35 @@ public class Novels {
 
     private final List<Novel> novels;
 
-    public boolean isEmpty() {
+    boolean isEmpty() {
         return novels.isEmpty();
     }
 
-    public void add(Novel novel) {
+    void add(Novel novel) {
         novels.add(novel);
     }
 
-    public int getSameParentSizeBy(NovelKey id) {
+    int getSameParentSizeBy(NovelKey id) {
         return (int) novels.stream().filter(i -> i.getParentNovelKey().equals(id)).count();
     }
 
-    public int size() {
+    int size() {
         return novels.size();
     }
 
-    public boolean existSameParent(NovelKey parentNovelKey) {
+    boolean existSameParent(NovelKey parentNovelKey) {
         return novels.stream().anyMatch(e->e.getParentNovelKey().equals(parentNovelKey));
     }
 
-    public boolean isNotExist(WriterId writerId) {
+    boolean isNotExist(WriterId writerId) {
         return novels.stream().noneMatch(n-> n.getWriterId().getId().equals(writerId.getId()));
     }
 
-    public boolean isNotExist(Title title) {
+    boolean isNotExist(Title title) {
         return novels.stream().noneMatch(n-> n.getTitle().equals(title));
     }
 
-    public Novel get(int index) {
+    Novel get(int index) {
         return novels.get(index);
     }
 }
