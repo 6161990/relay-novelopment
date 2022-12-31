2b04bb1bdff31f96d999a4ac67aff0eed03b3b0e
6161990 <6161990src@gmail.com>
2022-12-31 09:49:16 +0900

    자잘한 수정

--- domain/src/test/java/com/yoon/relayNovelopment/domain/NovelBoardTest.java
+++ domain/src/test/java/com/yoon/relayNovelopment/domain/NovelBoardTest.java
@@ -57,71 +57,71 @@ class NovelBoardTest {
     @DisplayName("fork->relay")
     @Test
     void relay_at_fork() {
-        NovelBoard novelStage = NovelBoardBuilderForTest.builder()
+        NovelBoard novelBoard = NovelBoardBuilderForTest.builder()
                 .id("id")
                 .opening(openingKey("id"), writerId("writer"), title("bang"), content("content"))
                 .relay(novel(relayKey("id2"), openingKey("id"), writerId("writer2"), title("bang2")))
                 .buildForRelay();
 
-        novelStage.fork(novel(relayKey("id3"), openingKey("id"), writerId("writer3"), title("bang4")));
+        novelBoard.fork(novel(relayKey("id3"), openingKey("id"), writerId("writer3"), title("bang4")));
 
-        novelStage.relay(novel(relayKey("id4"), parentKey("id3"), writerId("writer4"), title("bang5")));
+        novelBoard.relay(novel(relayKey("id4"), parentKey("id3"), writerId("writer4"), title("bang5")));
 
-        assertThat(novelStage.getNovelSize()).isEqualTo(3);
+        assertThat(novelBoard.getNovelSize()).isEqualTo(3);
     }
 
     @DisplayName("relay->fork->relay->fork")
     @Test
     void fork_at_relay_after_fork() {
-        NovelBoard novelStage = NovelBoardBuilderForTest.builder()
+        NovelBoard novelBoard = NovelBoardBuilderForTest.builder()
                 .id("id")
                 .opening(openingKey("id"), writerId("writer"), title("bang"), content("content"))
                 .relay(novel(relayKey("id2"), openingKey("id"), writerId("writer2"), title("bang2")))
                 .buildForRelay();
 
-        novelStage.fork(novel(relayKey("id3"), openingKey("id"), writerId("writer3"), title("bang4")));
-        novelStage.relay(novel(relayKey("id4"), parentKey("id3"), writerId("writer4"), title("bang5")));
-        novelStage.fork(novel(relayKey("id5"), parentKey("id3"), writerId("writer5"), title("bang6")));
+        novelBoard.fork(novel(relayKey("id3"), openingKey("id"), writerId("writer3"), title("bang4")));
+        novelBoard.relay(novel(relayKey("id4"), parentKey("id3"), writerId("writer4"), title("bang5")));
+        novelBoard.fork(novel(relayKey("id5"), parentKey("id3"), writerId("writer5"), title("bang6")));
 
-        assertThat(novelStage.getNovelSize()).isEqualTo(4);
+        assertThat(novelBoard.getNovelSize()).isEqualTo(4);
     }
 
     @Test
     void when_not_exist_same_parentNovel_add_fork() {
-        NovelBoard novelStage = NovelBoardBuilderForTest.builder()
+        NovelBoard novelBoard = NovelBoardBuilderForTest.builder()
                 .id("id")
                 .opening(openingKey("id"), writerId("writer"), title("bang"), content("content"))
                 .relay(novel(relayKey("id2"), openingKey("id"), writerId("writer2"), title("bang2")))
                 .buildForRelay();
 
-        assertThatThrownBy(()->novelStage.fork(novel(relayKey("id3"), openingKey("id2"), writerId("writer3"), title("bang4"))))
+        assertThatThrownBy(()->novelBoard.fork(novel(relayKey("id3"), openingKey("id2"), writerId("writer3"), title("bang4"))))
                 .isInstanceOf(NovelBoardException.class)
                 .hasMessage("Not Exist Same Parent Novel.");
     }
 
     @Test
-    void writer는_하나의_novelStage_에_중복하여_등록할_수_없다() {
-        NovelBoard novelStage = NovelBoardBuilderForTest.builder()
+    void writer는_하나의_novelBoard_에_중복하여_등록할_수_없다() {
+        NovelBoard novelBoard = NovelBoardBuilderForTest.builder()
                 .id("id")
                 .opening(openingKey("id"), writerId("writer"), title("bang"), content("content"))
                 .relay(novel(relayKey("id2"), openingKey("id"), writerId("writer2"), title("bang2")))
                 .buildForRelay();
 
-        assertThatThrownBy(()-> novelStage.relay(novel(relayKey("id3"), parentKey("id2"), writerId("writer2"), title("bang2"))))
+        assertThatThrownBy(()-> novelBoard.relay(novel(relayKey("id3"), parentKey("id2"), writerId("writer2"), title("bang2"))))
                 .isInstanceOf(NovelBoardException.class)
                 .hasMessageContaining("Already exist the writer");
 
     }
 
     @Test
-    void 하나의_novelStage_에_제목이_중복될_수_없다() {
-        NovelBoard novelStage = NovelBoardBuilderForTest.builder()
+    void 하나의_novelBoard_에_제목이_중복될_수_없다() {
+        NovelBoard novelBoard = NovelBoardBuilderForTest.builder()
                 .id("id")
                 .opening(openingKey("id"), writerId("writer"), title("bang"), content("content"))
                 .relay(novel(relayKey("id2"), openingKey("id"), writerId("writer2"), title("bang2")))
                 .buildForRelay();
 
-        assertThatThrownBy(()-> novelStage.relay(novel(relayKey("id3"), parentKey("id2"), writerId("writer3"), title("bang2"))))
+        assertThatThrownBy(()-> novelBoard.relay(novel(relayKey("id3"), parentKey("id2"), writerId("writer3"), title("bang2"))))
                 .isInstanceOf(NovelBoardException.class)
                 .hasMessageContaining("Already exist the title.");
     }
