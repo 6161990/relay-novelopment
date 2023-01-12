fc787cd4475cb5e35a246ea404135b7b4be5de59
YoonjiKim <6161990src@gmail.com>
2023-01-12 17:13:21 +0900

    fix

--- relay-novelopment-admin/admin-service/admin/src/main/kotlin/com/yoon/relayNovelopment/RelayNovelopmentAdminApplication.kt
+++ relay-novelopment-admin/admin-service/admin/src/main/kotlin/com/yoon/relayNovelopment/RelayNovelopmentAdminApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class RelayNovelopmentAdminApplication {
-    fun main(args: Array<String>){
-        runApplication<RelayNovelopmentAdminApplication>(*args)
-    }
+class RelayNovelopmentAdminApplication
+
+fun main(args: Array<String>){
+    runApplication<RelayNovelopmentAdminApplication>(*args)
 }
