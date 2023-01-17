465fff639d71df3c67b35ea818edbb23da70ea49
YoonjiKim <6161990src@gmail.com>
2023-01-17 15:39:02 +0900

    issue 상세 조회

--- relay-novelopment-admin/admin-service/admin/src/main/kotlin/com/yoon/relayNovelopment/service/IssueService.kt
+++ relay-novelopment-admin/admin-service/admin/src/main/kotlin/com/yoon/relayNovelopment/service/IssueService.kt
@@ -3,8 +3,10 @@ package com.yoon.relayNovelopment.service
 import com.yoon.relayNovelopment.domain.Issue
 import com.yoon.relayNovelopment.domain.IssueRepository
 import com.yoon.relayNovelopment.domain.enums.IssueStatus
+import com.yoon.relayNovelopment.exception.NotFoundException
 import com.yoon.relayNovelopment.model.IssueRequest
 import com.yoon.relayNovelopment.model.IssueResponse
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -32,5 +34,11 @@ class IssueService(
     fun getAll(status: IssueStatus): List<IssueResponse>? =
         issueRepository.findAllByStatusOrderByCreatedAtDesc(status)
             ?.map { IssueResponse(it) }
+
+    @Transactional(readOnly = true)
+    fun get(id: Long): IssueResponse {
+        val issue = issueRepository.findByIdOrNull(id) ?: throw NotFoundException("이슈가 존재하지 않습니다")
+        return IssueResponse(issue)
+    }
 
 }
--- relay-novelopment-admin/admin-service/admin/src/main/kotlin/com/yoon/relayNovelopment/web/IssueController.kt
+++ relay-novelopment-admin/admin-service/admin/src/main/kotlin/com/yoon/relayNovelopment/web/IssueController.kt
@@ -5,6 +5,7 @@ import com.yoon.relayNovelopment.domain.enums.IssueStatus
 import com.yoon.relayNovelopment.model.IssueRequest
 import com.yoon.relayNovelopment.service.IssueService
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -29,5 +30,10 @@ class IssueController(
         @RequestParam(required = false, defaultValue = "TODO") status: IssueStatus
     ) = issueService.getAll(status)
 
+    @GetMapping("/{id}")
+    fun get(
+        authUser: AuthUser,
+        @PathVariable("id") id : Long
+    ) = issueService.get(id)
 
 }
