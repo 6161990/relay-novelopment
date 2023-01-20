e252a7fb220b53dc37d5de37521ab7f1465794b5
6161990 <6161990src@gmail.com>
2023-01-20 13:24:25 +0900

    FIXME 설정

--- relay-novelopment/domain/src/main/java/com/yoon/relayNovelopment/domain/Novel.java
+++ relay-novelopment/domain/src/main/java/com/yoon/relayNovelopment/domain/Novel.java
@@ -7,7 +7,7 @@ import java.util.Objects;
 import static org.valid4j.Validation.validate;
 
 @Value(staticConstructor = "of")
-public class Novel {
+public class Novel { // FIXME novel 이 아니라 Article 이고, genre 를 novel 로 해야하지 않을까?
     RelayNovelKey relayNovelKey;
     NovelKey parentNovelKey;
     WriterId writerId;
